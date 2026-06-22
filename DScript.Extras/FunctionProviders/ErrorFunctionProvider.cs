503df18343eb7bfa2657e4f33742dff71836262b
bizzehdee <killallthehumans@gmail.com>
2026-06-22 23:30:34 +0100

    Error types
    Add Error, TypeError, RangeError, ReferenceError, SyntaxError, URIError, and EvalError
    constructor functions at root scope. Each returns an object with name, message, and
    stack properties, enabling instanceof-style error discrimination via e.name.

---
+++ DScript.Extras/FunctionProviders/ErrorFunctionProvider.cs
@@ -0,0 +1,86 @@
+/*
+Copyright (c) 2014 - 2020 Darren Horrocks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+namespace DScript.Extras.FunctionProviders
+{
+    [ScriptClass("Errors")]
+    public static class ErrorFunctionProvider
+    {
+        private static ScriptVar MakeError(string name, string message)
+        {
+            var obj = new ScriptVar(ScriptVar.Flags.Object);
+            obj.AddChild("name", new ScriptVar(name));
+            obj.AddChild("message", new ScriptVar(message));
+            obj.AddChild("stack", new ScriptVar(""));
+            return obj;
+        }
+
+        [ScriptMethod("Error", "msg", AppearAtRoot = true)]
+        public static void ErrorImpl(ScriptVar var, object userData)
+        {
+            var msgVar = var.GetParameter("msg");
+            var.ReturnVar = MakeError("Error", msgVar.IsUndefined ? "" : msgVar.String);
+        }
+
+        [ScriptMethod("TypeError", "msg", AppearAtRoot = true)]
+        public static void TypeErrorImpl(ScriptVar var, object userData)
+        {
+            var msgVar = var.GetParameter("msg");
+            var.ReturnVar = MakeError("TypeError", msgVar.IsUndefined ? "" : msgVar.String);
+        }
+
+        [ScriptMethod("RangeError", "msg", AppearAtRoot = true)]
+        public static void RangeErrorImpl(ScriptVar var, object userData)
+        {
+            var msgVar = var.GetParameter("msg");
+            var.ReturnVar = MakeError("RangeError", msgVar.IsUndefined ? "" : msgVar.String);
+        }
+
+        [ScriptMethod("ReferenceError", "msg", AppearAtRoot = true)]
+        public static void ReferenceErrorImpl(ScriptVar var, object userData)
+        {
+            var msgVar = var.GetParameter("msg");
+            var.ReturnVar = MakeError("ReferenceError", msgVar.IsUndefined ? "" : msgVar.String);
+        }
+
+        [ScriptMethod("SyntaxError", "msg", AppearAtRoot = true)]
+        public static void SyntaxErrorImpl(ScriptVar var, object userData)
+        {
+            var msgVar = var.GetParameter("msg");
+            var.ReturnVar = MakeError("SyntaxError", msgVar.IsUndefined ? "" : msgVar.String);
+        }
+
+        [ScriptMethod("URIError", "msg", AppearAtRoot = true)]
+        public static void URIErrorImpl(ScriptVar var, object userData)
+        {
+            var msgVar = var.GetParameter("msg");
+            var.ReturnVar = MakeError("URIError", msgVar.IsUndefined ? "" : msgVar.String);
+        }
+
+        [ScriptMethod("EvalError", "msg", AppearAtRoot = true)]
+        public static void EvalErrorImpl(ScriptVar var, object userData)
+        {
+            var msgVar = var.GetParameter("msg");
+            var.ReturnVar = MakeError("EvalError", msgVar.IsUndefined ? "" : msgVar.String);
+        }
+    }
+}
